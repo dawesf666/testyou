d2f5f7427b8168f4a68424934006427a0ba7da00
Adrian Voicu <adrian@myslut.tyb.net>
2024-03-11 15:11:38 +0200

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layuot = ({ children }: { children: React.ReactNode }) => {
+    return <main className="auth">{children}</main>;
+};
+
+export default Layuot;
---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
+
+import React from "react";
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <p>ProfilePage</p>
+            {/* <UserButton afterSignOutUrl="/" /> */}
+        </div>
+    );
+};
+
+export default ProfilePage;
